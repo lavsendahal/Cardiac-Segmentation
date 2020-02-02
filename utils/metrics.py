b5ae470aea8d3ede95a15abd1a087abe3d4b187b
Lavsen Dahal <er.lavsen@gmail.com>
2020-02-02 12:39:00 +0545

    Added Dice as Metric

--- utils/metrics.py
+++ utils/metrics.py
@@ -22,6 +22,13 @@ class Evaluator(object):
         MIoU = np.nanmean(MIoU)
         return MIoU
 
+    def Dice_Score(self):
+        Dice = 2*np.diag(self.confusion_matrix) / (
+                    np.sum(self.confusion_matrix, axis=1) + np.sum(self.confusion_matrix, axis=0))
+        Dice = np.nanmean(Dice)
+        return Dice	
+
+
     def Frequency_Weighted_Intersection_over_Union(self):
         freq = np.sum(self.confusion_matrix, axis=1) / np.sum(self.confusion_matrix)
         iu = np.diag(self.confusion_matrix) / (
