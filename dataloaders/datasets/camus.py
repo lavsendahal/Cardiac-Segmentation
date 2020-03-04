31ffa567d07969b2e4169d2d314234917c28e827
Lavsen Dahal <er.lavsen@gmail.com>
2020-03-04 11:54:56 +0545

    rotation as augmentation

--- dataloaders/datasets/camus.py
+++ dataloaders/datasets/camus.py
@@ -86,7 +86,7 @@ class CamusSegmentation(Dataset):
             tr.RandomHorizontalFlip(),
             tr.RandomScaleCrop(base_size=self.args.base_size, crop_size=self.args.crop_size),
             tr.RandomGaussianBlur(),
-            tr.RandomRotate(degree=25)
+            tr.RandomRotate(degree=30)
             tr.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
             tr.ToTensor()])
 
