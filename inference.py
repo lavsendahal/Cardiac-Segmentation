1d4c6ec176f641fe4cd6e98719f38b3c7c10524a
Lavsen Dahal <er.lavsen@gmail.com>
2020-07-13 09:20:51 +0545

    Inference_for_mc_dropout_sampling_strategy

--- inference.py
+++ inference.py
@@ -9,7 +9,12 @@ from tqdm import tqdm
 from PIL import Image
 from modeling.sync_batchnorm.replicate import patch_replication_callback
 
-def save_output_images(LOAD_PATH_MODEL,model, test_loader, SAVE_PATH_MODEL_OUTPUT, test_fn_list, sampling_strategy= None):
+def enable_dropout(m):
+  for m in model.modules():
+    if m.__class__.__name__.startswith('Dropout'):
+      m.train()
+
+def save_output_images(LOAD_PATH_MODEL, model, test_loader, SAVE_PATH_MODEL_OUTPUT, test_fn_list, sampling_strategy= None):
     '''Saves the prediction image from segmentation model in local disk. Supports only batch size 1 now.
     Args:
         LOAD_PATH_MODEL: Path where model weights are saved
@@ -18,12 +23,12 @@ def save_output_images(LOAD_PATH_MODEL,model, test_loader, SAVE_PATH_MODEL_OUTPU
         test_fn_list : List of filenames in test set
         sampling_strategy : Choice ['hse', 'dropout', 'augment', 'None']
     '''
-    # To Do Pytorch load_state_dict error in multi-gpu use
-    if args.sampling_strategy is None:        
-        checkpoint = torch.load(LOAD_PATH_MODEL)
-        model.load_state_dict(checkpoint['state_dict'])
+    # To Do Pytorch load_state_dict error in multi-gpu use due to different style of saving for best model and all models
+    if args.sampling_strategy == 'hse':      
+        model.load_state_dict(torch.load(LOAD_PATH_MODEL))   
     else:
-        model.load_state_dict(torch.load(LOAD_PATH_MODEL))    
+        checkpoint = torch.load(LOAD_PATH_MODEL)
+        model.load_state_dict(checkpoint['state_dict']) 
     model = model.to('cuda')
     model.eval()
 
@@ -36,15 +41,14 @@ def save_output_images(LOAD_PATH_MODEL,model, test_loader, SAVE_PATH_MODEL_OUTPU
                 output = output.data.cpu().numpy()  
             pred = output[0,:,:,:]
             pred = np.argmax(pred, axis=0)
-
             # pred[pred ==1] = 85 ; pred[pred ==2] = 170 ; pred[pred ==3] = 255 #Only for Visualization
             pred = Image.fromarray((pred).astype(np.uint8))
             pred.save(SAVE_PATH_MODEL_OUTPUT + test_fn_list[i][0] +   '.png')
     return None
 
-def create_folders(starting_epoch= None, path = None, no_samples=50): 
-    for i in range(starting_epoch,starting_epoch+ no_samples):
-        os.mkdir(path + 'epoch_' + str (i))
+def create_folders(starting_sample= None, path = None, no_samples=50): 
+    for i in range(starting_sample,starting_sample+ no_samples):
+        os.mkdir(path + 'sample_' + str (i))
 
 if __name__ == '__main__':
 
@@ -72,21 +76,29 @@ if __name__ == '__main__':
 
     if args.sampling_strategy == 'hse':
         ALL_MODELS_PATH_HSE = '/media/HDD1/lavsen/all_research/2d_echo_uncertainty/all_models/camus_dataset/'
-        model = torch.nn.DataParallel(model)  #Support for Parallel GPU in training
+        model = torch.nn.DataParallel(model)  #Support for Parallel GPU 
         patch_replication_callback(model)
         model = model.cuda()
-        starting_epoch = 50
+        starting_sample = 50
         base_save_path_output_hse = '/media/HDD1/lavsen/all_research/2d_echo_uncertainty/outputs/sampling_strategy/HSE/'
-        # TO DO - Create folders to save outputs if doesn't exist already
-        #create_folders(starting_epoch =starting_epoch , path = base_save_path_output_hse, no_samples= args.no_samples)
-
+        if not (os.path.isdir(base_save_path_output_hse +'sample_0' )) :
+            create_folders(starting_sample =starting_sample , path = base_save_path_output_hse, no_samples= args.no_samples)
         print ('Generating {} samples for HSE sampling strategy'.format(args.no_samples))
-        for i in range(0,args.no_samples):
+        for i in range(args.no_samples):
             LOAD_PATH_MODEL = ALL_MODELS_PATH_HSE + 'epoch_' + str(starting_epoch +i) 
-            current_sampling_no = starting_epoch +i
             save_path_model_outputs_hse = base_save_path_output_hse + 'epoch_' + str(starting_epoch +i)  + '/'
             save_output_images(LOAD_PATH_MODEL,model, test_loader, save_path_model_outputs_hse, test_fn_list, sampling_strategy= args.sampling_strategy)
 
     if args.sampling_strategy == 'dropout':
-        # TO DO
-        
+        LOAD_PATH_MODEL = '/home/lavsen/NAAMII/Projects/cardiac_seg/camus/pytorch-deeplab-xception/run/camus/deeplabv3plus-resnet-pretrained/model_best.pth.tar' 
+        base_save_path_output_dropout = '/media/HDD1/lavsen/all_research/2d_echo_uncertainty/outputs/sampling_strategy/dropout/'
+        print ('Generating {} samples for Dropout sampling strategy'.format(args.no_samples))
+        if not (os.path.isdir(base_save_path_output_dropout +'sample_0' )) :
+            create_folders(starting_sample =0 , path = base_save_path_output_dropout, no_samples= args.no_samples)
+        for i in range(args.no_samples):
+            save_path_model_outputs_dropout = base_save_path_output_dropout + 'sample_' + str(i)  + '/'
+            save_output_images(LOAD_PATH_MODEL,model, test_loader, save_path_model_outputs_dropout, test_fn_list, sampling_strategy= args.sampling_strategy)
+
+
+     if args.sampling_strategy == 'augment':
+         #To Do
